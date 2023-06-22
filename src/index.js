da52d87b9479ae3db834c8e0e42b1c9eddf1b753
vivian waithera <vivyannekori@gmail.com>
2023-06-23 00:05:37 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+let form = document.getElementById('create-task-form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    buildToDo(form.querySelector("#new-task-description").value)
+    // buildToDo(e.target.new-task-description.value)
+    form.reset()
+  })
+
+function buildToDo(todo) {
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'x'
+  li.textContent = `${todo} `
+  li.appendChild(btn)
+  document.getElementById('tasks').appendChild(li)
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove()
+}
